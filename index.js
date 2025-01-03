8c67f23531964b52bf0530a3b656259326da3de5
i-am-ajay <adinaj.edu@gmail.com>
2025-01-03 12:32:36 +0530

    added api endpoint to reset index

--- index.js
+++ index.js
@@ -52,6 +52,9 @@ app.get("/vessle/:position",(req,res)=>{
   
 });
 
+app.get("/vessle/reset",(req,res)=>{
+  index = 0
+})
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
